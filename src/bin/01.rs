1504ec05846b550d63d27af27b5c98b379c74e85
Mario Longobardi <mlongobardi2@bloomberg.net>
2022-12-01 15:21:58 -0500

    Format and lint

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -19,26 +19,33 @@ impl FromStr for Elf {
     type Err = ParseIntError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let parsed_snacks =
-            s.lines().map(|l| l.parse::<u32>()).collect::<Result<Vec<_>, _>>()?;
-        Ok(Elf { snacks: parsed_snacks })
+        let parsed_snacks = s
+            .lines()
+            .map(|l| l.parse::<u32>())
+            .collect::<Result<Vec<_>, _>>()?;
+        Ok(Elf {
+            snacks: parsed_snacks,
+        })
     }
 }
 
 type Input = Vec<Elf>;
 
 pub fn part_one(input: &str) -> Option<u32> {
-    let parsed_input = parse_input(&input);
+    let parsed_input = parse_input(input);
     most_calories_carried(&parsed_input)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    let parsed_input = parse_input(&input);
+    let parsed_input = parse_input(input);
     top3_calories_carried(&parsed_input)
 }
 
-fn parse_input(input : &str) -> Input {
-    input.split("\n\n").map(|e| e.parse::<Elf>().unwrap()).collect()
+fn parse_input(input: &str) -> Input {
+    input
+        .split("\n\n")
+        .map(|e| e.parse::<Elf>().unwrap())
+        .collect()
 }
 
 fn most_calories_carried(input: &Input) -> Option<u32> {
@@ -46,7 +53,14 @@ fn most_calories_carried(input: &Input) -> Option<u32> {
 }
 
 fn top3_calories_carried(input: &Input) -> Option<u32> {
-    Some(input.iter().map(Elf::count_calories).sorted_by_key(|c| Reverse(*c)).take(3).sum())
+    Some(
+        input
+            .iter()
+            .map(Elf::count_calories)
+            .sorted_by_key(|c| Reverse(*c))
+            .take(3)
+            .sum(),
+    )
 }
 
 fn main() {
@@ -65,16 +79,23 @@ mod tests {
             snacks: vec![7000, 8000, 9000],
         };
         assert_eq!(elf.count_calories(), 24000);
-
     }
     #[test]
     fn test_most_calories_carried() {
         let input = vec![
-            Elf { snacks: vec![1000, 2000, 3000] },
+            Elf {
+                snacks: vec![1000, 2000, 3000],
+            },
             Elf { snacks: vec![4000] },
-            Elf { snacks: vec![5000, 6000] },
-            Elf { snacks: vec![7000, 8000, 9000] },
-            Elf { snacks: vec![10000] },
+            Elf {
+                snacks: vec![5000, 6000],
+            },
+            Elf {
+                snacks: vec![7000, 8000, 9000],
+            },
+            Elf {
+                snacks: vec![10000],
+            },
         ];
         assert_eq!(most_calories_carried(&input), Some(24000));
     }
