05e3c2d2cbfce4d08cce1facbe4f937bb1201553
Mario Longobardi <mlongobardi2@bloomberg.net>
2022-12-15 23:01:54 -0500

    Before clippy

--- src/bin/15.rs
+++ src/bin/15.rs
@@ -1,10 +1,9 @@
 use anyhow::anyhow;
-use itertools::Itertools;
 use lazy_static::lazy_static;
 use regex::Regex;
-use z3::ast::Ast;
-use std::str::FromStr;
 use std::collections::HashSet;
+use std::str::FromStr;
+use z3::ast::Ast;
 
 #[derive(Debug, Clone, PartialEq, Eq, Default, Hash, PartialOrd, Ord)]
 pub struct Position {
@@ -46,24 +45,24 @@ impl FromStr for SensorReading {
             sensor: Position {
                 x: captures
                     .name("sx")
-                    .ok_or(anyhow!("Cannot extract Sensor[x]"))?
+                    .ok_or_else(|| anyhow!("Cannot extract Sensor[x]"))?
                     .as_str()
                     .parse::<i64>()?,
                 y: captures
                     .name("sy")
-                    .ok_or(anyhow!("Cannot extract Sensor[y]"))?
+                    .ok_or_else(|| anyhow!("Cannot extract Sensor[y]"))?
                     .as_str()
                     .parse::<i64>()?,
             },
             beacon: Position {
                 x: captures
                     .name("bx")
-                    .ok_or(anyhow!("Cannot extract Beacon[x]"))?
+                    .ok_or_else(|| anyhow!("Cannot extract Beacon[x]"))?
                     .as_str()
                     .parse::<i64>()?,
                 y: captures
                     .name("by")
-                    .ok_or(anyhow!("Cannot extract Beacon[y]"))?
+                    .ok_or_else(|| anyhow!("Cannot extract Beacon[y]"))?
                     .as_str()
                     .parse::<i64>()?,
             },
@@ -72,19 +71,25 @@ impl FromStr for SensorReading {
 }
 
 pub fn covered_per_row(input: &str, row_y: i64) -> Option<usize> {
-    let mut beacons : HashSet<Position> = HashSet::new();
-    let mut covered : HashSet<Position> = HashSet::new();
+    let mut beacons: HashSet<Position> = HashSet::new();
+    let mut covered: HashSet<Position> = HashSet::new();
     input
         .lines()
         .filter_map(|l| l.parse::<SensorReading>().ok())
         .for_each(|SensorReading { sensor, beacon }| {
             let dist = sensor.manhattan_distance(&beacon);
-            let mut marker = Position { x: sensor.x, y: row_y};
+            let mut marker = Position {
+                x: sensor.x,
+                y: row_y,
+            };
             while sensor.manhattan_distance(&marker) <= dist {
                 covered.insert(marker.clone());
                 marker.x += 1;
             }
-            let mut marker = Position { x: sensor.x, y: row_y};
+            let mut marker = Position {
+                x: sensor.x,
+                y: row_y,
+            };
             while sensor.manhattan_distance(&marker) <= dist {
                 covered.insert(marker.clone());
                 marker.x -= 1;
@@ -93,51 +98,12 @@ pub fn covered_per_row(input: &str, row_y: i64) -> Option<usize> {
                 beacons.insert(beacon);
             }
         });
-    Some(covered.difference(&beacons).count())
-}
-
-pub fn find_beacon_slow(input: &str, search_space: i64) -> Option<i64> {
-    let mut candidates : HashSet<Position> = HashSet::new();
-    let squares : Vec<(Position, i64)> = input
-        .lines()
-        .filter_map(|l| l.parse::<SensorReading>().ok())
-        .map(|SensorReading { sensor, beacon }| {
-            let dist = sensor.manhattan_distance(&beacon) + 1;
-            for i in 0..=dist {
-                let candidate = Position{x: sensor.x + dist - i, y: sensor.y + i};
-                assert_eq!(candidate.manhattan_distance(&sensor), dist);
-                if candidate.x >= 0 && candidate.x <= search_space && candidate.y >= 0 && candidate.y <= search_space {
-                    candidates.insert(candidate);
-                }
-                let candidate = Position{x: sensor.x - dist + i, y: sensor.y - i};
-                assert_eq!(candidate.manhattan_distance(&sensor), dist);
-                if candidate.x >= 0 && candidate.x <= search_space && candidate.y >= 0 && candidate.y <= search_space {
-                    candidates.insert(candidate);
-                }
-                let candidate = Position{x: sensor.x + i, y: sensor.y + dist - i};
-                assert_eq!(candidate.manhattan_distance(&sensor), dist);
-                if candidate.x >= 0 && candidate.x <= search_space && candidate.y >= 0 && candidate.y <= search_space {
-                    candidates.insert(candidate);
-                }
-                let candidate = Position{x: sensor.x - i, y: sensor.y - dist + i};
-                assert_eq!(candidate.manhattan_distance(&sensor), dist);
-                if candidate.x >= 0 && candidate.x <= search_space && candidate.y >= 0 && candidate.y <= search_space {
-                    candidates.insert(candidate);
-                }
-            }
-            (sensor, dist - 1)
-        }).collect();
-    for candidate in candidates {
-        if squares.iter().all(|(sensor, dist)| candidate.manhattan_distance(sensor) > *dist) {
-            return Some(4000000*candidate.x + candidate.y);
-        }
-    }
-    None
+    Some(covered.len() - beacons.len())
 }
 
 fn abs<'a>(val: z3::ast::Int<'a>) -> z3::ast::Int<'a> {
     let zero = z3::ast::Int::from_i64(val.get_ctx(), 0);
-    val.gt(&zero).ite(&val, &(- &val))
+    val.gt(&zero).ite(&val, &(-&val))
 }
 
 fn within_range<'a>(val: &z3::ast::Int<'a>, low: i64, high: i64) -> z3::ast::Bool<'a> {
@@ -156,19 +122,20 @@ pub fn find_beacon(input: &str, search_space: i64) -> Option<i64> {
     solver.assert(&within_range(&goal_y, 0, search_space));
     for reading in input
         .lines()
-        .filter_map(|l| l.parse::<SensorReading>().ok()) {
-            let x = ast::Int::from_i64(&ctx, reading.sensor.x);
-            let y = ast::Int::from_i64(&ctx, reading.sensor.y);
-            let strength = ast::Int::from_i64(&ctx, reading.strength());
-            solver.assert(&(abs(&goal_x - x) + abs(&goal_y - y)).gt(&strength));
+        .filter_map(|l| l.parse::<SensorReading>().ok())
+    {
+        let x = ast::Int::from_i64(&ctx, reading.sensor.x);
+        let y = ast::Int::from_i64(&ctx, reading.sensor.y);
+        let strength = ast::Int::from_i64(&ctx, reading.strength());
+        solver.assert(&(abs(&goal_x - x) + abs(&goal_y - y)).gt(&strength));
     }
     if solver.check() != SatResult::Sat {
-        return None
+        return None;
     }
     let model = solver.get_model()?;
     let xv = model.eval(&goal_x, true)?.as_i64()?;
     let yv = model.eval(&goal_y, true)?.as_i64()?;
-    Some(4000000*xv + yv)
+    Some(4000000 * xv + yv)
 }
 
 pub fn part_one(input: &str) -> Option<usize> {
