7ecf2b4a40e9816ef875232a226476790a6bfd6c
Mario Longobardi <mlongobardi2@bloomberg.net>
2022-12-27 16:31:37 -0500

    Days 23-25: Prepare

---
+++ src/bin/24.rs
@@ -0,0 +1,30 @@
+pub fn part_one(input: &str) -> Option<u32> {
+    None
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    None
+}
+
+fn main() {
+    let input = &advent_of_code::read_file("inputs", 24);
+    advent_of_code::solve!(1, part_one, input);
+    advent_of_code::solve!(2, part_two, input);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let input = advent_of_code::read_file("examples", 24);
+        assert_eq!(part_one(&input), Some(18));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let input = advent_of_code::read_file("examples", 24);
+        assert_eq!(part_two(&input), None);
+    }
+}
